072f4bac2b493d4bdb42c0bd3e9704de6a087a9d
Mariam Nersisyan <mariamnersisyan97@gmail.com>
2022-05-03 17:51:17 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+  function onBlur() {
+    console.log("Hey! Eyes on me!");
+  }
+  function onFocus() {
+    console.log("Good!");
+  }
+  return (
+    <>
+      <button onBlur={onBlur} onFocus={onFocus}>
+        Eyes on me
+      </button>
+    </>
+  );
+}
+
+export default EyesOnMe;
